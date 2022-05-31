80095bd14a6114d3bd07190de3a847c8d850c140
TienCongTC7 <congnt.bi11-047@st.usth.edu.vn>
2022-05-31 13:11:57 +0700

    finish prac3

---
+++ server.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <unistd.h>
+
+
+
+int main(int argc, char *argv[]){
+    struct sockaddr_in saddr, caddr;
+    int sockfd, clen, clientfd;
+    unsigned short port = 8764;
+    sockfd = socket(AF_INET,SOCK_STREAM,0);
+    if((sockfd < 0)){
+        printf("Error creating socket\n");
+        return 1;
+    }
+
+    memset(&saddr,0, sizeof(saddr));
+    saddr.sin_family = AF_INET;
+    saddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    saddr.sin_port = htons(port);
+    if(bind(sockfd, (struct sockaddr *) &saddr,sizeof(saddr)) < 0){
+        printf("Error binding");
+        return 1;
+    }
+
+    if(listen(sockfd,5)<0){
+        printf("Error listening\n");
+    }
+    clen = sizeof(caddr);
+    if((clientfd=accept(sockfd, (struct sockaddr *) &caddr, &clen))<0){
+        printf("Error accepting connection \n");
+        return 1;
+    }
+    return 0;
+
+}
